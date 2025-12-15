776313725b983fbbdceb4cf610ac2e4965c0e8ad
Haysansan <haysan.eang@student.cadt.edu.kh>
2025-12-16 04:43:26 +0700

    PauseMainMenuV2

--- Assets/Final Project/Scripts/Systems/UI/PauseMenu.cs
+++ Assets/Final Project/Scripts/Systems/UI/PauseMenu.cs
@@ -1,23 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
-
+    [Header("Pause Menu")]
     public GameObject pauseMenu;
     public static bool isPaused;
-    // Start is called before the first frame update
+
+    [Header("Button Colors")]
+    public Color normalButtonColor = new Color(1, 1, 1, 0); // Transparent
+    public Color hoverButtonColor = Color.white;
+    public Color normalTextColor = Color.white;
+    public Color hoverTextColor = Color.black;
+
     void Start()
     {
         pauseMenu.SetActive(false);
+
+        // Initialize all buttons in the pause menu
+        Button[] buttons = pauseMenu.GetComponentsInChildren<Button>(true);
+        foreach (Button btn in buttons)
+        {
+            ButtonSetup(btn);
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             if (pauseMenu.activeInHierarchy)
@@ -31,12 +45,47 @@ public class PauseMenu : MonoBehaviour
         }
     }
 
+    void ButtonSetup(Button btn)
+    {
+        // Ensure button has Image and Text
+        Image btnImage = btn.GetComponent<Image>();
+        TextMeshProUGUI btnText = btn.GetComponentInChildren<TextMeshProUGUI>();
+
+        // Set default colors
+        if (btnImage != null) btnImage.color = normalButtonColor;
+        if (btnText != null) btnText.color = normalTextColor;
+
+        // Add hover events
+        EventTrigger trigger = btn.gameObject.GetComponent<EventTrigger>();
+        if (trigger == null)
+            trigger = btn.gameObject.AddComponent<EventTrigger>();
+
+        // Pointer Enter
+        EventTrigger.Entry entryEnter = new EventTrigger.Entry();
+        entryEnter.eventID = EventTriggerType.PointerEnter;
+        entryEnter.callback.AddListener((data) => {
+            if (btnImage != null) btnImage.color = hoverButtonColor;
+            if (btnText != null) btnText.color = hoverTextColor;
+        });
+        trigger.triggers.Add(entryEnter);
+
+        // Pointer Exit
+        EventTrigger.Entry entryExit = new EventTrigger.Entry();
+        entryExit.eventID = EventTriggerType.PointerExit;
+        entryExit.callback.AddListener((data) => {
+            if (btnImage != null) btnImage.color = normalButtonColor;
+            if (btnText != null) btnText.color = normalTextColor;
+        });
+        trigger.triggers.Add(entryExit);
+    }
+
     public void PauseGame()
     {
-       pauseMenu.SetActive(true);
+        pauseMenu.SetActive(true);
         Time.timeScale = 0f;
         isPaused = true;
     }
+
     public void ResumeGame()
     {
         pauseMenu.SetActive(false);
@@ -44,17 +93,6 @@ public class PauseMenu : MonoBehaviour
         isPaused = false;
     }
 
-    //public void OpenOptions()
-    //{
-    //    pauseMenu.SetActive(false);
-    //    optionsMenu.SetActive(true);
-    //}
-
-    //public void CloseOptions()
-    //{
-    //    optionsMenu.SetActive(false);
-    //    pauseMenu.SetActive(true);
-    //}
     public void MainMenu()
     {
         Time.timeScale = 1f;
@@ -66,7 +104,6 @@ public class PauseMenu : MonoBehaviour
         Time.timeScale = 1f;
         SceneManager.LoadScene("Tutorial");
     }
-
 
     public void Exit()
     {
